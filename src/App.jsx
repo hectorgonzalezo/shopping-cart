f37849388d7ced1b5c1c70a6c763038fd6a98542
Hector Gonzalez <gorozco.hector@gmail.com>
2022-10-10 10:28:30 -0400

    Add router for home and shop

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Shop from './components/Shop';
+import './styles/appStyle.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="shop" element={<Shop />} />
         </Routes>
       </BrowserRouter>
-      <Footer />
+      <Footer projectName="shopping-cart" />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/headerStyle.css';
 
 function Header() {
   return (
     <header>
       <h1>This is the header</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>Cart</li>
+      </ul>
     </header>
   );
 }
